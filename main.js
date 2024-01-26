7258e66342baf37404808b70df6f7d00faa13441
aravinth.r <aravinth.r@zohocorp.com>
2024-01-26 20:54:46 +0530

    [CHORE] New message

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const http = require('http');
 
 http.createServer((req, res)=>{
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end('Hello Aravinth!');
+    res.end('Hello Abi, epo veetuku vara or already veetla irundhu tha indha message padikuriya??!');
 }).listen(8000);
