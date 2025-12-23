29b6ae37b274ab5d069d4bfcd6ced30ea7a1e38f
aravinth.r <aravinth.r@zohocorp.com>
2025-12-23 09:33:13 +0530

    chore: fix deploy issues

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ const http = require('http');
 http.createServer((req, res)=>{
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end('Hello World app by Aravinth Ramesh');
-}).listen(8000);
+}).listen(80);
